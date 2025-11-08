6125996276dfd5211918a4d65800cd3c72e865bc
Wilfred Reign <wilfredr319n@gmail.com>
2025-11-08 16:30:32 +0200

    refactor: simplify RegisterWithUs component structure and remove unnecessary imports

--- src/app/enroll-with-us/page.tsx
+++ src/app/enroll-with-us/page.tsx
@@ -1,36 +1,16 @@
-"use client"
-import React from 'react'
-import RootLayout from '../layout'
-import { Paper } from '@mui/material'
-import NavigationBar from '@/components/navigation-components/NavigationBar'
-import ServerLayout from '../serverLayout'
-import Footer from '@/components/footer-components/Footer'
-import EnrollmentPage from '@/components/enrollment-form-components/EnrollmentPage'
+"use client";
 
-const RegisterWithUs = () => {
+import { Paper } from "@mui/material";
+import NavigationBar from "@/components/navigation-components/NavigationBar";
+import EnrollmentPage from "@/components/enrollment-form-components/EnrollmentPage";
+import Footer from "@/components/footer-components/Footer";
+
+export default function RegisterWithUs() {
     return (
-        <ServerLayout>
-            <RootLayout>
-                <Paper
-                    sx={{
-                        minHeight: "100vh",
-                        height: "100%",
-                        width: "100%",
-                        overflow: "hidden",
-                        borderRadius: "0",
-                        margin: 0,
-                        // bgcolor: "#3a4254ff",
-                        backgroundImage: `linear-gradient( to bottom,#3a4254ff,rgba(8, 12, 40, 1))`,
-                    }}
-                >
-                    <NavigationBar />
-                    <EnrollmentPage />
-                    <Footer />
-                </Paper>
-            </RootLayout>
-        </ServerLayout>
-    )
-
+        <Paper sx={{ minHeight: "100vh", width: "100%", backgroundImage: `linear-gradient(to bottom,#3a4254ff,rgba(8, 12, 40, 1))` }}>
+            <NavigationBar />
+            <EnrollmentPage />
+            <Footer />
+        </Paper>
+    );
 }
-
-export default RegisterWithUs
