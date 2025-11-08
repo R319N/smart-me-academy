28672c1b5bd86890e47523e66edcec4f94dd13d8
Wilfred Reign <wilfredr319n@gmail.com>
2025-11-08 21:53:28 +0200

    amend 3

--- src/app/enroll-with-us/page.tsx
+++ src/app/enroll-with-us/page.tsx
@@ -4,13 +4,26 @@ import { Paper } from "@mui/material";
 import NavigationBar from "@/components/navigation-components/NavigationBar";
 import EnrollmentPage from "@/components/enrollment-form-components/EnrollmentPage";
 import Footer from "@/components/footer-components/Footer";
+import { Suspense } from "react";
 
 export default function RegisterWithUs() {
-    return (
-        <Paper sx={{ minHeight: "100vh", width: "100%", backgroundImage: `linear-gradient(to bottom,#3a4254ff,rgba(8, 12, 40, 1))` }}>
-            <NavigationBar />
-            <EnrollmentPage />
-            <Footer />
-        </Paper>
-    );
+  return (
+    <Paper
+      sx={{
+        minHeight: "100vh",
+        width: "100%",
+        backgroundImage:
+          `linear-gradient(to bottom,#3a4254ff,rgba(8, 12, 40, 1))`
+      }}
+    >
+      <NavigationBar />
+
+      {/* ✅ Suspense wrapper required because EnrollmentPage uses useSearchParams */}
+      <Suspense fallback={<div />}>
+        <EnrollmentPage />
+      </Suspense>
+
+      <Footer />
+    </Paper>
+  );
 }
